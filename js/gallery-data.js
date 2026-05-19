983a646b5a10898a611a243834738779a2b9a4ce
Kamal-Aya <assoukamal@hotmail.it>
2026-05-19 19:16:55 +0200

    Add gallery data for various categories

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -1,1 +1,43 @@
-bjbjbjb
+const galleryData = [
+
+    // GEBETE
+    {
+        category: "gebete",
+        src: "images/gebete/gebet1.jpg",
+        title: "Gebet 1"
+    },
+       {
+        category: "gebete",
+        src: "images/gebete/gebet2.jpg",
+        title: "Gebet Raum"
+    },
+
+    // AKTIVITAETEN
+    {
+        category: "aktivitaeten",
+        src: "images/aktivitaeten/kurs1.jpg",
+        title: "Deutschkurs"
+    },
+
+    // AUSFLUEGE
+    {
+        category: "ausfluege",
+        src: "images/ausfluege/ausflug1.jpg",
+        title: "Ausflug"
+    },
+
+    // EVENTS
+    {
+        category: "events",
+        src: "images/events/event1.jpg",
+        title: "Event"
+    },
+
+    // RAMADAN
+    {
+        category: "ramadan",
+        src: "images/ramadan/iftar1.jpg",
+        title: "Iftar"
+    }
+
+];
